2de994b67f0dd8bc85a9bdd225f0e87a6bfa2d2a
luciana <lpiazzi@despegar.com>
2018-10-25 12:56:15 -0300

    closures y corrutinas

---
+++ luavm/closures.lua
@@ -0,0 +1,7 @@
+-- CLOSURES
+
+-- Iterador
+function values (t)
+local i = 0
+return function ()
+end
---
+++ luavm/corroutines-basics.lua
@@ -0,0 +1,9 @@
+-- Creación
+co = coroutine.create(function () print("hi") end)
+print(co)
+
+-- chequear status
+print(coroutine.status(co))
+
+-- Ejecutar
+coroutine.resume(co)
