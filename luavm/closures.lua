477147c3bb5d339d85dba869627468059bd4b3ef
luciana <lpiazzi@despegar.com>
2018-10-25 12:56:54 -0300

    wip

--- luavm/closures.lua
+++ luavm/closures.lua
@@ -2,6 +2,16 @@
 
 -- Iterador
 function values (t)
-local i = 0
-return function ()
+	local i = 0
+	return function () i = i + 1; return t[i]
+end
+
+t = {10, 20, 30}
+iter = values(t)
+-- creates the iterator
+while true do
+	local element = iter()
+	-- calls the iterator
+	if element == nil then break end
+		print(element)
 end
